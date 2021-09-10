50bf7f876386c35d05cd9746d2f3bc4d1bcf7007
praveen <praveenthakur388@gmail.com>
2021-09-10 17:54:51 +0530

    Add project files.

---
+++ News/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace News
+{
+    public static class Settings
+    {
+        public static string NewsApiKey
+        {
+            get
+            {
+                return "<Your API Key>";
+            }
+        }
+    }
+}
